c82af012eb59d7916c35cc682c1d988461d3391b
Glenn Greibesland <glenn@glenng.no>
2017-04-26 22:02:41 +0200

    fix app-toolbar tests

--- src/app/app-toolbar/app-toolbar.component.spec.ts
+++ src/app/app-toolbar/app-toolbar.component.spec.ts
@@ -2,6 +2,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { MaterialModule } from '@angular/material';
 
 import { AppToolbarComponent } from './app-toolbar.component';
 
@@ -11,9 +12,10 @@ describe('AppToolbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppToolbarComponent ]
+      declarations: [AppToolbarComponent],
+      imports: [MaterialModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
